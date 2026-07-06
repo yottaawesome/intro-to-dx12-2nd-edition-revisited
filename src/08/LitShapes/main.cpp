3cd5da7ff5d6ee53e2daa0da9db366fe10b9278c
Vasilios Magriplis <etameson@gmail.com>
2026-07-07 09:09:11 +1000

    Minor changes.

--- src/08/LitShapes/main.cpp
+++ src/08/LitShapes/main.cpp
@@ -5,7 +5,7 @@ import litshapes;
 // Required exports for DX12-Agility SDK
 // https://devblogs.microsoft.com/directx/gettingstarted-dx12agility/
 // Make sure this is in sync with the nuget package version.
-extern "C" { __declspec(dllexport) extern const auto D3D12SDKVersion = 619u; }
+extern "C" { __declspec(dllexport) extern const Win32::UINT D3D12SDKVersion = 619u; }
 extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\"; }
 
 #pragma comment(lib,"d3dcompiler.lib")
