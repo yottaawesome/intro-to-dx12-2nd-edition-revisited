bbb22eb4e0e7a0bd8cf7e6943121095aa21afe67
Vasilios Magriplis <etameson@gmail.com>
2026-07-08 09:30:04 +1000

    Start working on next chapters.

---
+++ src/09/TexWaves/main.cpp
@@ -0,0 +1,7 @@
+import std;
+import shared;
+
+auto wWinMain(Win32::HINSTANCE hInstance, Win32::HINSTANCE, Win32::LPWSTR lpCmdLine, int) -> int
+{
+    return 0;
+}
---
+++ src/09/TexturedShapes/main.cpp
@@ -0,0 +1,24 @@
+import std;
+import shared;
+
+// Required exports for DX12-Agility SDK
+// https://devblogs.microsoft.com/directx/gettingstarted-dx12agility/
+// Make sure this is in sync with the nuget package version.
+extern "C" { __declspec(dllexport) extern const Win32::UINT D3D12SDKVersion = 619u; }
+extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\"; }
+
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "dxcompiler.lib") // dxc
+
+auto wWinMain(Win32::HINSTANCE hInstance, Win32::HINSTANCE, Win32::LPWSTR, int) -> int
+try
+{
+    return 0;
+}
+catch (const DxException& e)
+{
+    Win32::MessageBoxW(nullptr, e.ToString().c_str(), L"HR Failed", Win32::MbOk);
+    return 0;
+}
