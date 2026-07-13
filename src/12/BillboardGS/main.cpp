cf93250485d517aa2b3b4c91ac3f3e93ac025527
Vasilios Magriplis <etameson@gmail.com>
2026-07-14 05:48:32 +1000

    Port BillboardGS.

--- src/12/BillboardGS/main.cpp
+++ src/12/BillboardGS/main.cpp
@@ -1,7 +1,28 @@
 import std;
 import shared;
+import billboard;
+
+// Required exports for DX12-Agility SDK
+// https://devblogs.microsoft.com/directx/gettingstarted-dx12agility/
+// Make sure this is in sync with the nuget package version.
+extern "C" { __declspec(dllexport) extern const Win32::UINT D3D12SDKVersion = 619u; }
+extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\"; }
+
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "dxcompiler.lib") // dxc
 
 auto wWinMain(Win32::HINSTANCE hInstance, Win32::HINSTANCE, Win32::LPWSTR, int) -> int
+try
 {
-	return 0;
+	if (IsDebugBuild)
+		Win32::_CrtSetDbgFlag(Win32::CrtAllocMemDf | Win32::CrtLeakCheckDf);
+	auto theApp = BillboardApp{ hInstance };
+	return theApp.Run();
+}
+catch (const std::exception& e)
+{
+	Win32::MessageBoxW(nullptr, AnsiToWString(e.what()).c_str(), L"Error", Win32::MbOk);
+	return -1;
 }
