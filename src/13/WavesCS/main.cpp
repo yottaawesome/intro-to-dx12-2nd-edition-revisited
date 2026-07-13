c6abdbe11f63a5fb319bb282d49ac59f5ae72f1d
Vasilios Magriplis <etameson@gmail.com>
2026-07-14 07:44:47 +1000

    Beging work on chapter 13.

---
+++ src/13/WavesCS/main.cpp
@@ -0,0 +1,18 @@
+import std;
+import shared;
+
+// Required exports for DX12-Agility SDK
+// https://devblogs.microsoft.com/directx/gettingstarted-dx12agility/
+// Make sure this is in sync with the nuget package version.
+extern "C" { __declspec(dllexport) extern const Win32::UINT D3D12SDKVersion = 619u; }
+extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\"; }
+
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "dxcompiler.lib") // dxc
+
+auto wWinMain(Win32::HINSTANCE hInstance, Win32::HINSTANCE, Win32::LPWSTR, int nCmdShow) -> int
+{
+	return 0;
+}
