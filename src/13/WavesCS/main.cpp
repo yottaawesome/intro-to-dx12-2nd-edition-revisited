6d1ad1ab14b324f6b836aefe95039192a230db13
Vasilios Magriplis <etameson@gmail.com>
2026-07-19 17:01:56 +1000

    Begin work on WavesCS.

--- src/13/WavesCS/main.cpp
+++ src/13/WavesCS/main.cpp
@@ -1,10 +1,10 @@
+import std;
+import shared;
+
 #pragma comment(lib,"d3dcompiler.lib")
 #pragma comment(lib, "D3D12.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "dxcompiler.lib") // dxc
-
-import std;
-import shared;
 
 // Required exports for DX12-Agility SDK
 // https://devblogs.microsoft.com/directx/gettingstarted-dx12agility/
@@ -13,6 +13,12 @@ extern "C" { __declspec(dllexport) extern const Win32::UINT D3D12SDKVersion = 61
 extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\"; }
 
 auto wWinMain(Win32::HINSTANCE hInstance, Win32::HINSTANCE, Win32::LPWSTR, int nCmdShow) -> int
+try
 {
+	return 0;
+}
+catch (const std::exception& e)
+{
+	Win32::MessageBoxW(nullptr, AnsiToWString(e.what()).c_str(), L"Error", Win32::MbOk);
 	return 0;
 }
