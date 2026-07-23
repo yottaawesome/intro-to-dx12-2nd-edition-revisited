ebd488a7550bdd09544f50fb62c948bba3d344ae
Vasilios Magriplis <etameson@gmail.com>
2026-07-23 14:11:24 +1000

    Clean up.

--- src/10/BlendDemo/main.cpp
+++ src/10/BlendDemo/main.cpp
@@ -1,17 +1,17 @@
-import std;
-import shared;
-import blenddemo;
-
-// Required exports for DX12-Agility SDK
-// https://devblogs.microsoft.com/directx/gettingstarted-dx12agility/
-// Make sure this is in sync with the nuget package version.
-extern "C" { __declspec(dllexport) extern const Win32::UINT D3D12SDKVersion = 619u; }
-extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\"; }
-
 #pragma comment(lib,"d3dcompiler.lib")
 #pragma comment(lib, "D3D12.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "dxcompiler.lib") // dxc
+
+// Required exports for DX12-Agility SDK
+// https://devblogs.microsoft.com/directx/gettingstarted-dx12agility/
+// Make sure this is in sync with the nuget package version.
+extern "C" { __declspec(dllexport) extern const unsigned int D3D12SDKVersion = 619u; }
+extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\"; }
+
+import std;
+import shared;
+import blenddemo;
 
 auto wWinMain(Win32::HINSTANCE hInstance, Win32::HINSTANCE, Win32::LPWSTR, int) -> int
 try
